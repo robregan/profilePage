a05da0005f0a61af814d1dbbd8c79ebfa47142ae
rob <robert_regan86@yahoo.com>
2022-12-07 07:52:58 -0500

    initial commit

---
+++ src/components/profile/ProfileList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ProfileList.module.css'
+
+const ProfileList = () => {
+  return (
+    <div>
+      <h1>ProfileList</h1>
+    </div>
+  )
+}
+
+export default ProfileList
